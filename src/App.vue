3e1fa00397d34497ae999a77cf0050e6306a5813
cran-cg <guptachiranjeev.13@gmail.com>
2020-02-17 18:41:44 +0530

    chore(app): add basic skeleton to the todo app Bootswatch: Superhero theme is used FontAwesome icons are used

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="todo">
+    <div class="page-header">
+      <h1>TODO:</h1>
+      <p>simple, offline TODO app, powered by Vue.js ❤️</p>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#todo {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+
+.page-header h1 {
+  font-weight: 300;
+}
+</style>
